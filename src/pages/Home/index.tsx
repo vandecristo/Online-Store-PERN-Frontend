fac1e17eeae30d1b00c0b048dc993f659e076908
Alexandr Khalenkov <aleksandr.halenkov@fojin.tech>
2022-07-19 17:26:54 +0300

    Start Home page and fix token refresh

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import React, {useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
 
 import { Context } from '../../index';
 import { MobxStores } from '../../../interfaces';
@@ -7,7 +8,7 @@ import { fetchBrands, fetchDevices, fetchTypes } from '../../http/deviceAPI';
 
 import styles from './styles.module.scss';
 
-const Home = () => {
+const Home = observer(() => {
     const { deviceStore } = useContext<MobxStores>(Context);
 
     useEffect(() => {
@@ -17,39 +18,56 @@ const Home = () => {
     }, []);
 
     return (
-        <div>
-            <div>Search</div>
-            <div className={styles.shop__item}>
+        <div className={styles.home}>
+            <span className={styles.home__title}>Search</span>
+            <div className={styles.home__item_slider}>
                 <Slider/>
             </div>
-            <div>
-                <span>Categories:</span>
-                {deviceStore.types?.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            {item.name}
-                        </div>
-                    )
-                })}
+            <div className={styles.home__itemWrapper}>
+                <div className={styles.home__item}>
+                    <span className={styles.home__title}>Categories:</span>
+                    <div className={styles.home__categoryWrapper}>
+                        {deviceStore.types?.map((item) => {
+                            return (
+                                <div key={item.id} className={styles.home__category}>
+                                    {item.name}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
-            <div>Reviews</div>
-            <div>News</div>
-            <div>
-                Brands
-                <div>
-                    <span>Categories:</span>
-                    {deviceStore.brands?.map((item) => {
-                        return (
-                            <div key={item.id}>
-                                {item.name}
-                            </div>
-                        )
-                    })}
+            <div className={styles.shop__itemWrapper}>
+                <div className={styles.shop__item}>
+                    <span>Reviews:</span>
+                </div>
+            </div>
+            <div className={styles.shop__itemWrapper}>
+                <div className={styles.shop__item}>
+                    <span>News:</span>
+                </div>
+            </div>
+            <div className={styles.shop__itemWrapper}>
+                <div className={styles.shop__item}>
+                    <span>Brands:</span>
+                </div>
+
+                <div className={styles.shop__itemWrapper}>
+                    <div className={styles.shop__item}>
+                        <span>Categories:</span>
+                        {deviceStore.brands?.map((item) => {
+                            return (
+                                <div key={item.id}>
+                                    {item.name}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
             <div>Subscription</div>
         </div>
     );
-};
+});
 
 export default Home;
